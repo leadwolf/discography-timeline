3765ecd01e828ce12d39430dd52344e6d6a33943
Christopher Caroni <christophercaroni97@gmail.com>
2018-10-13 22:47:44 +0200

    feat(Login): create page layout & style

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import React from 'react';
 
 import { ThemeProvider } from './views/components/app/ThemeProvider';
 import Navbar from './views/components/navigation/Navbar';
+import Login from './views/pages/Login';
 
 const App = () => (
     <ThemeProvider>
         <Navbar />
+        <Login />
     </ThemeProvider>
 );
 
---
+++ src/views/pages/Login.jsx
@@ -0,0 +1,33 @@
+import './login.scss';
+
+import Button from '@material-ui/core/Button';
+import { withStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import React from 'react';
+
+const styles = {
+    button: {},
+    textBody: {
+        textAlign: 'center',
+    },
+};
+
+function Login({ classes }) {
+    return (
+        <div className="page-login--container">
+            <div className="page-login--content-container">
+                <Typography variant="h5">Please login with Spotify</Typography>
+
+                <Typography variant="body1" className={classes.textBody}>
+                    This application uses your Spotify account to search artists and albums
+                </Typography>
+
+                <Button variant="contained" color="primary" className={classes.button}>
+                    Login
+                </Button>
+            </div>
+        </div>
+    );
+}
+
+export default withStyles(styles)(Login);
