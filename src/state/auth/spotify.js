cedb5a3c18af9b1d26b4cd0091f6d89abbe75499
Christopher Caroni <christophercaroni97@gmail.com>
2018-10-14 14:03:37 +0200

    feat(spotify-login): init api

---
+++ src/state/auth/spotify.js
@@ -0,0 +1,16 @@
+import SpotifyWebApi from 'spotify-web-api-node';
+
+const redirectUri = 'http://localhost:3000/login/callback';
+const clientId = 'd77b2c91e2354ad4b65d4b62d7214631';
+const state = 'some-state-of-my-choice';
+
+// Setting credentials can be done in the wrapper's constructor, or using the API object's setters.
+const spotifyApi = new SpotifyWebApi({
+    redirectUri,
+    clientId,
+});
+
+// Create the authorization URL
+const authorizeURL = spotifyApi.createAuthorizeURL([], state);
+
+export { authorizeURL };
