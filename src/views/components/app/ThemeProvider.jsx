de06652cf890ee70c50c2e3d101a226b62bd4e5b
Christopher Caroni <christophercaroni97@gmail.com>
2018-10-14 13:48:08 +0200

    feat(theme): add CssBaseline

--- src/views/components/app/ThemeProvider.jsx
+++ src/views/components/app/ThemeProvider.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import blue from '@material-ui/core/colors/blue';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 const theme = createMuiTheme({
     palette: {
@@ -10,7 +11,9 @@ const theme = createMuiTheme({
 });
 
 const ThemeProvider = ({ children }) => (
-    <MuiThemeProvider theme={theme}>{children}</MuiThemeProvider>
+    <MuiThemeProvider theme={theme}>
+        <CssBaseline>{children}</CssBaseline>
+    </MuiThemeProvider>
 );
 
 export { ThemeProvider };
