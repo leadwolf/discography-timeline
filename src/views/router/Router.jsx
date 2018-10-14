d42aa124e9c9866b94c6d50a550e00f068df6faa
Christopher Caroni <christophercaroni97@gmail.com>
2018-10-14 14:24:52 +0200

    feat(LoginResult): create skeleton page with route

--- src/views/router/Router.jsx
+++ src/views/router/Router.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import { Redirect, Route, Router as ReactRouter, Switch } from 'react-router-dom';
 
 import { history } from '../../state/history';
-import Login from '../pages/Login';
+import { Login, LoginResult } from '../pages';
 
 const Router = () => (
     <ReactRouter history={history}>
         <Switch>
             <Route exact path="/login" component={Login} />
+            <Route exact path="/login/callback" component={LoginResult} />
 
             <Redirect to="/login" component={Login} />
         </Switch>
