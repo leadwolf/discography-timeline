4cea1ed543578a982e0c3a8342a4769d4fe149da
Christopher Caroni <christophercaroni97@gmail.com>
2018-10-15 20:27:30 +0200

    refactor(ArtistAvatar): remove react-avatar, use custom style

--- src/views/components/Artists/ArtistAvatar/ArtistAvatar.jsx
+++ src/views/components/Artists/ArtistAvatar/ArtistAvatar.jsx
@@ -2,18 +2,10 @@ import './artistAvatar.scss';
 
 import Typography from '@material-ui/core/Typography';
 import React from 'react';
-import Avatar from 'react-avatar';
 import Tooltip from '@material-ui/core/Tooltip';
 
 import { withStyles } from '@material-ui/core/styles';
 import { artistItemPropType } from './types';
-
-const avatarProps = {
-    round: true,
-    textSizeRatio: 2,
-    size: '15vw',
-    initials: name => name,
-};
 
 const styles = theme => ({
     name: {
@@ -25,9 +17,16 @@ const styles = theme => ({
 
 const ArtistAvatar = ({ classes, artist: { name, images } }) => {
     const avatar = () => {
-        if (images.length > 0) return <Avatar {...avatarProps} src={images[0].url} />;
+        if (images.length > 0)
+            return <img className="artist-avatar-img" alt={name} src={images[0].url} />;
 
-        return <Avatar {...avatarProps} name={name} />;
+        return (
+            <div className="artist-avatar-img artist-avatar-text-container">
+                <div className="artist-avatar-text-content">
+                    <Typography className={classes.name}>{name}</Typography>
+                </div>
+            </div>
+        );
     };
 
     return (
