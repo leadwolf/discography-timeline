a2333b8e3500338bbab89011968d307f763f0e20
Christopher Caroni <christophercaroni97@gmail.com>
2018-10-16 22:44:18 +0200

    feat(albums): show albums in Artist page

--- src/views/pages/Artist/Artist.jsx
+++ src/views/pages/Artist/Artist.jsx
@@ -7,6 +7,7 @@ import { withRouter } from 'react-router-dom';
 import ReactRouterPropTypes from 'react-router-prop-types';
 
 import { operations } from '../../../state/artists';
+import { operations as albumOperations } from '../../../state/albums';
 
 class Artist extends React.Component {
     componentDidMount() {
@@ -15,9 +16,11 @@ class Artist extends React.Component {
                 params: { id },
             },
             searchArtist,
+            searchArtistAlbums,
         } = this.props;
 
         searchArtist(id);
+        searchArtistAlbums(id);
     }
 
     render() {
@@ -25,12 +28,26 @@ class Artist extends React.Component {
             artists: {
                 selectedArtist: { name },
             },
+            albums: { items },
         } = this.props;
 
         return (
             <div className="page-artist-container">
                 <div className="page-artist-name-container">
                     <Typography variant="h2">{name}</Typography>
+                </div>
+
+                <div className="page-artist-albums-container">
+                    <div className="page-artist-albums-title-container">
+                        <Typography variant="h2">Albums</Typography>
+                    </div>
+                    <div className="page-artist-albums-content-container">
+                        {items.map(item => (
+                            <Typography key={item.id} variant="h5">
+                                {item.name}
+                            </Typography>
+                        ))}
+                    </div>
                 </div>
             </div>
         );
@@ -41,10 +58,11 @@ Artist.propTypes = {
     match: ReactRouterPropTypes.match.isRequired,
 };
 
-const mapStateToProps = state => ({ artists: state.artists });
+const mapStateToProps = state => ({ artists: state.artists, albums: state.albums });
 
 const mapDispatchToProps = dispatch => ({
     searchArtist: id => dispatch(operations.getSingleArtist(id)),
+    searchArtistAlbums: id => dispatch(albumOperations.search(id)),
 });
 
 const ConnectedArtist = withRouter(
