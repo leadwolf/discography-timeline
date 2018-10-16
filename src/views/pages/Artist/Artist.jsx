81761dc190c68abc61e02e35bc76ca85aa8f2ec3
Christopher Caroni <christophercaroni97@gmail.com>
2018-10-16 21:36:22 +0200

    feat(Artist): add name

--- src/views/pages/Artist/Artist.jsx
+++ src/views/pages/Artist/Artist.jsx
@@ -1,6 +1,12 @@
+import './artist.scss';
+
+import Typography from '@material-ui/core/Typography';
 import React from 'react';
+import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import ReactRouterPropTypes from 'react-router-prop-types';
+
+import { operations } from '../../../state/artists';
 
 class Artist extends React.Component {
     componentDidMount() {
@@ -8,11 +14,26 @@ class Artist extends React.Component {
             match: {
                 params: { id },
             },
+            searchArtist,
         } = this.props;
+
+        searchArtist(id);
     }
 
     render() {
-        return <div>todo</div>;
+        const {
+            artists: {
+                selectedArtist: { name },
+            },
+        } = this.props;
+
+        return (
+            <div className="page-artist-container">
+                <div className="page-artist-name-container">
+                    <Typography variant="h2">{name}</Typography>
+                </div>
+            </div>
+        );
     }
 }
 
@@ -20,6 +41,17 @@ Artist.propTypes = {
     match: ReactRouterPropTypes.match.isRequired,
 };
 
-const ConnectedArtist = withRouter(Artist);
+const mapStateToProps = state => ({ artists: state.artists });
+
+const mapDispatchToProps = dispatch => ({
+    searchArtist: id => dispatch(operations.getSingleArtist(id)),
+});
+
+const ConnectedArtist = withRouter(
+    connect(
+        mapStateToProps,
+        mapDispatchToProps
+    )(Artist)
+);
 
 export { ConnectedArtist as Artist };
