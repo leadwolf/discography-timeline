18732827cdd21764e9a8e0710c664e21a4c88496
Christopher Caroni <christophercaroni97@gmail.com>
2018-10-20 21:46:12 +0200

    refactor(AlbumTypeFilter): use chips

---
+++ src/utils/albumUtils.js
@@ -0,0 +1,11 @@
+export const ALBUM_GROUP_ALBUM = 'album';
+export const ALBUM_GROUP_SINGLE = 'single';
+export const ALBUM_GROUP_COMPILATION = 'compilation';
+export const ALBUM_GROUP_APPEARS_ON = 'appears_on';
+
+export const ALBUM_GROUP_TYPES = [
+    ALBUM_GROUP_ALBUM,
+    ALBUM_GROUP_SINGLE,
+    ALBUM_GROUP_COMPILATION,
+    ALBUM_GROUP_APPEARS_ON,
+];
--- src/views/components/Albums/AlbumTypeFilter/AlbumTypeFilter.jsx
+++ src/views/components/Albums/AlbumTypeFilter/AlbumTypeFilter.jsx
@@ -1,4 +1,6 @@
+import Chip from '@material-ui/core/Chip';
 import FormControl from '@material-ui/core/FormControl';
+import Input from '@material-ui/core/Input';
 import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
 import Select from '@material-ui/core/Select';
@@ -6,31 +8,70 @@ import { withStyles } from '@material-ui/core/styles';
 import PropTypes from 'prop-types';
 import React from 'react';
 
+import { ALBUM_GROUP_TYPES } from '../../../../utils/albumUtils';
+
 const styles = theme => ({
     formControl: {
         margin: theme.spacing.unit,
         minWidth: 120,
+        maxWidth: 300,
+    },
+    chips: {
+        display: 'flex',
+        flexWrap: 'wrap',
+    },
+    chip: {
+        margin: theme.spacing.unit / 4,
     },
 });
 
-const AlbumTypeFilter = ({ classes, filter, handleChange }) => {
-    return (
-        <FormControl className={classes.formControl}>
-            <InputLabel htmlFor="album_types">Album Type</InputLabel>
-            <Select
-                multiple
-                value={filter}
-                onChange={handleChange}
-                inputProps={{ name: 'album_types', id: 'album_types' }}
-            >
-                <MenuItem value="album">Album</MenuItem>
-                <MenuItem value="single">Single</MenuItem>
-                <MenuItem value="appears_on">Appears on</MenuItem>
-                <MenuItem value="compilation">Compilation</MenuItem>
-            </Select>
-        </FormControl>
-    );
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+    PaperProps: {
+        style: {
+            maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+            width: 250,
+        },
+    },
 };
+
+const names = ALBUM_GROUP_TYPES;
+
+const AlbumTypeFilter = ({ classes, theme, filter, handleChange }) => (
+    <FormControl className={classes.formControl}>
+        <InputLabel htmlFor="album_types">Chip</InputLabel>
+        <Select
+            multiple
+            value={filter}
+            onChange={handleChange}
+            input={<Input name="album_types" id="album_types" />}
+            renderValue={selected => (
+                <div className={classes.chips}>
+                    {selected.map(value => (
+                        <Chip key={value} label={value} className={classes.chip} />
+                    ))}
+                </div>
+            )}
+            MenuProps={MenuProps}
+        >
+            {names.map(name => (
+                <MenuItem
+                    key={name}
+                    value={name}
+                    style={{
+                        fontWeight:
+                            filter.indexOf(name) === -1
+                                ? theme.typography.fontWeightRegular
+                                : theme.typography.fontWeightMedium,
+                    }}
+                >
+                    {name}
+                </MenuItem>
+            ))}
+        </Select>
+    </FormControl>
+);
 
 AlbumTypeFilter.propTypes = {
     classes: PropTypes.object.isRequired,
@@ -43,6 +84,6 @@ AlbumTypeFilter.defaultProps = {
     handleChange: () => undefined,
 };
 
-const StyledAlbumTypeFilter = withStyles(styles)(AlbumTypeFilter);
+const StyledAlbumTypeFilter = withStyles(styles, { withTheme: true })(AlbumTypeFilter);
 
 export { StyledAlbumTypeFilter as AlbumTypeFilter };
