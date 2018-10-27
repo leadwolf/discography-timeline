ee44b716face1caa9ae24ccf0ad638c2d01ee7af
Christopher Caroni <christophercaroni97@gmail.com>
2018-10-27 18:23:58 +0200

    feat(album): creat AlbumDate

---
+++ src/views/components/Albums/AlbumDate/AlbumDate.jsx
@@ -0,0 +1,19 @@
+import './albumDate.scss';
+
+import React from 'react';
+import moment from 'moment';
+import Typography from '@material-ui/core/Typography';
+
+import PropTypes from 'prop-types';
+
+const AlbumDate = ({ album: { release_date } }) => {
+    return <div className="album-timeline-date">{moment(release_date).format('LL')}</div>;
+};
+
+AlbumDate.propTypes = {
+    album: PropTypes.shape({
+        release_date: PropTypes.string.isRequired,
+    }).isRequired,
+};
+
+export { AlbumDate };
