1afdb9935500f377678a0eca839f3aa77fef9fdd
Christopher Caroni <christophercaroni97@gmail.com>
2018-10-27 18:24:03 +0200

    feat(album): create timeline

--- src/views/components/Albums/AlbumList/AlbumList.jsx
+++ src/views/components/Albums/AlbumList/AlbumList.jsx
@@ -1,15 +1,31 @@
+import 'react-vertical-timeline-component/style.min.css';
+
 import PropTypes from 'prop-types';
 import React from 'react';
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
 
 import { Album } from '../Album';
+import { AlbumDate } from '../AlbumDate';
+import { AlbumIcon } from '../AlbumIcon';
 import { albumType } from '../types';
 
 const AlbumList = ({ albums }) => {
     return (
         <div className="album-list-container">
-            {albums.map(album => (
+            <VerticalTimeline>
+                {albums.map(album => (
+                    <VerticalTimelineElement
+                        key={album.id}
+                        icon={<AlbumIcon album={album} />}
+                        date={<AlbumDate album={album} />}
+                    >
+                        <Album album={album} />
+                    </VerticalTimelineElement>
+                ))}
+            </VerticalTimeline>
+            {/* {albums.map(album => (
                 <Album key={album.id} album={album} />
-            ))}
+            ))} */}
         </div>
     );
 };
