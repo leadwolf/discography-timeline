76d9440451dfa0a870d5d1ea4f56c22cb3852692
Christopher Caroni <christophercaroni97@gmail.com>
2018-10-28 12:51:07 +0100

    feat(Album): show alts in expansion panel

--- src/views/components/Albums/Album/Album.jsx
+++ src/views/components/Albums/Album/Album.jsx
@@ -1,6 +1,13 @@
 import './album.scss';
 
 import Chip from '@material-ui/core/Chip';
+import Divider from '@material-ui/core/Divider';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import { withStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import PropTypes from 'prop-types';
 import React from 'react';
 import { withRouter } from 'react-router-dom';
@@ -9,21 +16,36 @@ import { ArtistLink } from '../../Artists/ArtistLink';
 import { AlbumLink } from '../AlbumLink';
 import { albumType } from '../types';
 
+const styles = theme => ({
+    expansionPanel: {
+        backgroundColor: '#ffffff',
+    },
+    expandIcon: {
+        color: '#000000',
+    },
+});
+
 const Album = ({
-    album: { name, album_type, total_tracks, artists },
+    album: { name, album_type, total_tracks, artists, alternatives = [] },
     showType,
     album,
-    match: {
-        params: { id: currentArtistId },
+    mini,
+    hideArtistName,
+    match: { params: { id: currentArtistId } } = {
+        params: {
+            id: '',
+        },
     },
+    classes,
 }) => {
     return (
         <div className="album-timeline-container">
-            <div className="album-timeline-title">{name}</div>
+            <div className={`album-timeline-title ${mini ? 'mini' : ''}`}>{name}</div>
 
             {artists.map(
                 artist =>
-                    artist.id !== currentArtistId && <ArtistLink key={artist.id} artist={artist} />
+                    artist.id !== currentArtistId &&
+                    !hideArtistName && <ArtistLink key={artist.id} artist={artist} />
             )}
 
             <div className="album-timeline-track-count">{`${total_tracks} track${
@@ -37,6 +59,32 @@ const Album = ({
                     <Chip label={album_type} color="primary" />
                 </div>
             )}
+
+            {alternatives.length > 0 && (
+                <div className="alternatives-container">
+                    <ExpansionPanel classes={{ root: classes.expansionPanel }}>
+                        <ExpansionPanelSummary
+                            expandIcon={<ExpandMoreIcon classes={{ root: classes.expandIcon }} />}
+                        >
+                            <Typography>
+                                {`${alternatives.length} alternative version${
+                                    alternatives.length > 1 ? 's' : ''
+                                }  (explicit, deluxe...)`}
+                            </Typography>
+                        </ExpansionPanelSummary>
+                        <ExpansionPanelDetails classes={{ root: 'alternatives-content-container' }}>
+                            {alternatives.map((alt, index) => (
+                                <React.Fragment key={alt.id}>
+                                    <Album album={alt} showType={showType} mini hideArtistName />
+                                    {index < alternatives.length - 1 && (
+                                        <Divider classes={{ root: 'divider' }} />
+                                    )}
+                                </React.Fragment>
+                            ))}
+                        </ExpansionPanelDetails>
+                    </ExpansionPanel>
+                </div>
+            )}
         </div>
     );
 };
@@ -44,8 +92,15 @@ const Album = ({
 Album.propTypes = {
     album: albumType.isRequired,
     showType: PropTypes.bool.isRequired,
+    mini: PropTypes.bool,
+    hideArtistName: PropTypes.bool,
 };
 
-const ConnectedALbum = withRouter(Album);
+Album.defaultProps = {
+    mini: false,
+    hideArtistName: false,
+};
+
+const ConnectedALbum = withRouter(withStyles(styles)(Album));
 
 export { ConnectedALbum as Album };
