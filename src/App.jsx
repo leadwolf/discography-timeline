af94b4e0ca3531afe8bee35752c4aaf7b083ac55
Christopher Caroni <christophercaroni97@gmail.com>
2018-11-10 18:59:59 +0100

    core: setup moment-duration-format

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.scss';
 
+import moment from 'moment';
 import React from 'react';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
@@ -8,6 +9,10 @@ import { configureStore } from './state/store';
 import { ThemeProvider } from './views/components/app/ThemeProvider';
 import { Loading } from './views/pages';
 import { Router } from './views/router';
+
+const momentDurationFormatSetup = require('moment-duration-format');
+
+momentDurationFormatSetup(moment);
 
 const { store, persistor } = configureStore();
 const App = () => (
